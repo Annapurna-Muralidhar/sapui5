91c8963ae97da905a79fe4daf19cf657847c5c16
Annapurna <pedditiannapurna@gmail.com>
2024-02-29 12:37:54 +0530

    step 4

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,9 @@
 //The "Hello World" text is now displayed by a SAPUI5 control
 //Instead of using native JavaScript to display a dialog we want to use a simple SAPUI5 control. Controls are used to define appearance and behavior of parts of the screen.
+//package.json->npm init
+//ui5.yaml->ui5 init
+
+
 sap.ui.define([
 	"sap/m/Text"
 ], (Text) => {
@@ -13,4 +17,12 @@ sap.ui.define([
 
 
 
-//contains application logic
+// js file contains contains application logic
+
+/**SAPUI5 supports multiple view types (XML, HTML, JavaScript).
+ *  When working with UI5, we recommend the use of XML,
+ *  as this produces the most readable code and will
+ *  force us to separate the view declaration from the controller logic.
+ *  Yet the look of our UI will not change. 
+ * 
+ * */
