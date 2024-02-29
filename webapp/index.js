47ad4ab83a79b8bffabdf584aa1e6d3cfc00f16d
Annapurna <pedditiannapurna@gmail.com>
2024-02-29 12:45:52 +0530

    step 3

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,10 @@
 //The "Hello World" text is now displayed by a SAPUI5 control
 //Instead of using native JavaScript to display a dialog we want to use a simple SAPUI5 control. Controls are used to define appearance and behavior of parts of the screen.
 sap.ui.define([
-	"sap/m/Text"
+	"sap/m/Text"//sap namespace for text control
 ], (Text) => {
 	"use strict";
-
+    //create new object
 	new Text({
 		text: "Hello World"
 	}).placeAt("content");
