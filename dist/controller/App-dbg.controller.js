df7d98d5536ed72762576887f35336812b566f96
Annapurna <pedditiannapurna@gmail.com>
2024-03-05 15:36:47 +0530

    step 38

---
+++ dist/controller/App-dbg.controller.js
@@ -0,0 +1,63 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+ 
+ ], (Controller) => {
+    "use strict";
+ 
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
+    });
+ });
+ 
+ /**We add an onInit function to the controller. This is one of SAPUI5’s
+  *  lifecycle methods that is invoked by the framework when the controller is created, 
+  * similar to the constructor of a control.
+
+Inside the function we instantiate a JSON model. 
+The data for the model only contains a single property for the “recipient”, 
+and inside this it also contains one additional property for the name.
+
+To be able to use this model from within the XML view,
+ we call the setModel function on the view and pass on our 
+ newly created model. The model is now set on the view.
+
+The message toast is just showing the static "Hello World" message.
+ We will show how to load a translated text here in the next step.*/
+
+ /**In the onInit function we instantiate the ResourceModel that points to the new message
+  *  bundle file where our texts are now located (i18n.properties file). The bundle name 
+  * ui5.walkthrough.i18n.i18n consists of the application namespace ui5.walkthrough (the application root 
+  * as defined in the index.html), the folder name i18n and finally the file name i18n without extension. 
+  * The SAPUI5 runtime calculates the correct path to the resource; in this case the path to our i18n.properties file.
+  *  Next, the model instance is set on the view as a named model with the key i18n. You use named models when you need to have several 
+  * models available in parallel.
+
+In the onShowHello event handler function we access
+ the i18n model to get the text from the message bundle file and
+  replace the placeholder {0} with the recipient from our data model. 
+  The getProperty method can be called in any model and takes the data path
+   as an argument. In addition, the resource bundle has a specific getText
+    method that takes an array of strings as second argument.
+
+The resource bundle can be accessed with the getResourceBundle method 
+of a ResourceModel. Rather than concatenating translatable texts manually,
+ we can use the second parameter of getText to replace parts of the text with non-static data.
+  During runtime, SAPUI5 tries to load the correct i18n_*.properties file based on your browser
+   settings and your locale. In our case we have only created one i18n.properties file to make 
+   it simple. However, you can see in the network traffic of your browser’s developer tools that 
+   SAPUI5 tries to load one or more i18n_*.properties files before falling back to the default 
+   i18n.properties file. */
+
+   /**The resource model for internationalization is called the i18n model.
+
+The default filename is i18n.properties.
+
+Resource bundle keys are written in (lower) camelCase.
+
+Resource bundle values can contain parameters like {0}, {1}, {2}, …
+
+Never concatenate strings that are translated, always use placeholders.
+
+Use Unicode escape sequences for special characters. */
---
+++ dist/controller/InvoiceList-dbg.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+    "../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel,formatter,Filter,FilterOperator) => {
+	"use strict";
+    
+	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+		formatter:formatter,
+        onInit() {
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
+
+		onFilterInvoices(oEvent) {
+			// build filter array
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			const oList = this.byId("invoiceList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+
+
+			  /*We load two new dependencies for the filtering. The filter object will hold our configuration for the filter action and the FilterOperator is a helper type that we need in order to specify the filter.
+
+In the onFilterInvoices function we construct a filter object from the search string that the user has typed in the search field. Event handlers always receive an event argument that can be used to access the parameters that the event provides. In our case the search field defines a parameter query that we access by calling getParameter("query") on the oEvent parameter.
+
+If the query is not empty, we add a new filter object to the still empty array of filters. However, if the query is empty, we filter the binding with an empty array. This makes sure that we see all list elements again. We could also add more filters to the array, if we wanted to search more than one data field. In our example, we just search in the ProductName path and specify a filter operator that will search for the given query string.
+
+The list is accessed with the ID that we have specified in the view, because the control is automatically prefixed by the view ID, we need to ask the view for the control with the helper function byId. On the list control we access the binding of the aggregation items to filter it with our newly constructed filter object. This will automatically filter the list by our search string so that only the matching items are shown when the search is triggered. The filter operator FilterOperator.Contains is not case-sensitive.*/
+		},
+
+		onPress(oEvent) {
+			const oItem = oEvent.getSource();
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail", {
+				invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+			});
+		}
+	});
+});
